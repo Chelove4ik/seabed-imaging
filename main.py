828f2150b28cc6a3206a125aa6d8a81aa7b8d4f6
Corey1337 <kolikendr@gmail.com>
2022-06-24 21:57:29 +1000

    main ui

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import putenv
 import sys
 
 from PyQt5.QtWidgets import QApplication
@@ -6,9 +7,9 @@ from windows.main_window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     w = MainWindow()
     w.move(300, 300)
+    w.setFixedSize(700, 615)
     w.setWindowTitle('Simple')
     w.show()
     sys.exit(app.exec_())
