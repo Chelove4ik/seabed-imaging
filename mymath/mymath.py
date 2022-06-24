cc94b302740ad8d937f893998322527a4d75053d
Anikald <roslyakov.vo@students.dvfu.ru>
2022-06-24 22:11:44 +1000

    testForDENIS

--- mymath/mymath.py
+++ mymath/mymath.py
@@ -6,27 +6,24 @@ raws = 1000
 columns = 8680
 
 
-def function43(test, a):
+def function43(test, arr):
     image = Image.new("RGB", (columns, raws))
     height = image.size[1]
     width = image.size[0]
     draw = ImageDraw.Draw(image)  # Создаем инструмент для рисования
     for x in range(height):
-        mean = numpy.mean(a[x])
+        mean = numpy.mean(arr[x])
         for y in range(width):
             try:
-                1 / a[x][y]
+                1 / arr[x][y]
                 sr = int((255 * math.log(round((test - 1) / mean + 1), test)))
                 draw.point((y, x), (sr, sr, sr))
             except ZeroDivisionError:
                 sr = int(0.4)
                 draw.point((y, x), (sr, sr, sr))
     return image
-    # image.save('test.jpg', "JPEG")
-    # image.show()
 
-
-def function364(li, a):
+def function364(li, arr):
     image = Image.new("RGB", (columns, raws))
     draw = ImageDraw.Draw(image)  # Создаем инструмент для рисования
     height = image.size[1]
@@ -39,21 +36,13 @@ def function364(li, a):
     for x in range(height):
         for y in range(width):
             try:
-                1 / a[x][y]
-                sr = int((2 * math.pi * li) / (c * ji) * math.sqrt(li * li - ld * ld) * a[x][y] * math.exp(
+                1 / arr[x][y]
+                sr = int((2 * math.pi * li) / (c * ji) * math.sqrt(li * li - ld * ld) * arr[x][y] * math.exp(
                     2 * mu * li) / 100000)
                 draw.point((y, x), (sr, sr, sr))
             except ZeroDivisionError:
                 sr = int(0.4)
                 draw.point((y, x), (sr, sr, sr))
     return image
-    # image.save('test1.jpg', "JPEG")
-    # image.show()
 
 
-def readFile(path):
-    f = open(path, 'r')
-    arr = []
-    for i in range(raws):
-        arr.append([float(n) for n in (f.readline().split())])
-    return arr
