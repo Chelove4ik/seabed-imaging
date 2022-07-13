ea2dc0a9b74193e48ca37e97e3a314f5a86027be
Denis <dennis220802@gmail.com>
2022-07-13 18:33:01 +1000

    смена виджета при выборе функции

--- windows/image_window.py
+++ windows/image_window.py
@@ -12,5 +12,17 @@ class ImageWindow(QMainWindow):
         self.array = array
         self.draw_image()
 
+        self.ui.w_log.hide()
+        self.ui.rb_lin.clicked.connect(self.change_current_widget)
+        self.ui.rb_log.clicked.connect(self.change_current_widget)
+
+    def change_current_widget(self):
+        if self.ui.rb_lin.isChecked():
+            self.ui.w_log.hide()
+            self.ui.w_lin.show()
+        if self.ui.rb_log.isChecked():
+            self.ui.w_lin.hide()
+            self.ui.w_log.show()
+
     def draw_image(self):
         pass
